7f5f9cfcf1552c5c84c93ee07fba84730f8ff4f2
丁丁历险记 <james.song007@hotmail.com>
2019-03-13 14:16:19 +0800

    tt

--- src/main/java/com/changinye/www/eureka/CenterEurekaApplication.java
+++ src/main/java/com/changinye/www/eureka/CenterEurekaApplication.java
@@ -1,13 +1,21 @@
 package com.changinye.www.eureka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CenterEurekaApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(CenterEurekaApplication.class);
+	private static String name = "Eureka注册中心";
 
 	public static void main(String[] args) {
 		SpringApplication.run(CenterEurekaApplication.class, args);
+		logger.info("【启动微服务成功】【{}】", name);
 	}
 
 }
