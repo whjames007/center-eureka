d9a4ca184fd687716b0e269101a32ca6a73f28e6
丁丁历险记 <james.song007@hotmail.com>
2019-03-13 09:00:43 +0800

    Initial commit

---
+++ src/main/java/com/changinye/www/eureka/CenterEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.changinye.www.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/changinye/www/eureka/CenterEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.changinye.www.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CenterEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
